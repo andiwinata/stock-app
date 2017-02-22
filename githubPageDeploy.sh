2ce509ed11935a6ca694cde642b6c070df943302
Andi <andi.winata@hotmail.com>
2017-02-22 20:39:18 +1100

    Removing unnecessary double cloning repository

--- githubPageDeploy.sh
+++ githubPageDeploy.sh
@@ -18,14 +18,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 echo "Repository: $SSH_REPO , last SHA: $SHA"
-echo
-
-echo "Cloning repo......"
-echo
-
-# Clone another repository to ./out
-git clone $REPO ./out
-cd out
 
 echo
 echo "Overwriting $TARGET_BRANCH with $SOURCE_BRANCH......"
@@ -74,7 +66,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 cp deploy_key ~/.ssh/id_rsa
 
